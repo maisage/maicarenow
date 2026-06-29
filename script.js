432bedf01a4465aa2b0c0cde4e9e27b80248d9d4
maisage <maitlindsey@gmail.com>
2026-06-29 01:23:52 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll(".story-card").forEach((card) => {
-    card.addEventListener("touchstart", () => { card.style.transform = "scale(0.99)"; }, { passive: true });
-    card.addEventListener("touchend", () => { card.style.transform = ""; }, { passive: true });
-  });
-});
